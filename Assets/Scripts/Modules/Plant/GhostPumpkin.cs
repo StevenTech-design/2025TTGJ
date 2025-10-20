242a2baef46a0d0750ed2003dd495906556065cb
Steven <1469359779@qq.com>
2025-10-21 01:55:29 +0900

    fix pumpkin

--- Assets/Scripts/Modules/Plant/GhostPumpkin.cs
+++ Assets/Scripts/Modules/Plant/GhostPumpkin.cs
@@ -5,36 +5,41 @@ using DG.Tweening;
 using TTGJ.Buff;
 using TTGJ.Common;
 using TTGJ.GamePlay;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace TTGJ.Plant
 {
     public class GhostPumpkin : PlantBase
     {
-        [SerializeField]
-        private float intervalTimer = 0.5f;
-        private float currentTime;
         private GameObject currentEatObject;
+
+        [SerializeField] private float interval = 0.3f;
+        [SerializeField] private float force = 10;
+        private Tween blinkTween;
+
+        
+
+        void Toggle()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
         
 
         private void Flashing() { 
-            transform.DOScale(Vector3.one * 1.5f, 1).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
+            blinkTween = DOVirtual.DelayedCall(interval, Toggle, false)
+                .SetLoops(-1, LoopType.Restart);
         }
 
         public override void OnEat()
         {
             base.OnEat();
-            HeadFlashBuff flashBuff = BuffManager.Instance.AddBuff<HeadFlashBuff>(PlayerController.Instance.transform,false);
-            ReplaceHeadBuff replaceHeadBuff = BuffManager.Instance.AddBuff<ReplaceHeadBuff>(PlayerController.Instance.transform,false);
             RevealBuff revealBuff = BuffManager.Instance.AddBuff<RevealBuff>(PlayerController.Instance.transform,false);
-            flashBuff.StartBuff();
-            replaceHeadBuff.StartBuff();
             revealBuff.StartBuff();
         }
         public override void OnTeleport(Transform baseTeleportPos)
         {
             base.OnTeleport(baseTeleportPos);
+            blinkTween?.Kill();
         }
         public override void OnMature()
         {
@@ -46,16 +51,20 @@ namespace TTGJ.Plant
             base.OnHarvest();
             transform.DOKill();
         }
-        private async Task OnTriggerEnter(Collider other) {
-            if (currentState != PlantState.Harvest ||other.gameObject.CompareTag("Field") || other.gameObject.layer == LayerMask.NameToLayer("Building") || currentEatObject != null) { 
+        private async void OnCollisionEnter(Collision collision) {
+            if (currentState != PlantState.Harvest 
+            ||collision.gameObject.CompareTag("Field") 
+            || collision.gameObject.layer == LayerMask.NameToLayer("Building") 
+            || currentEatObject != null) { 
                 return;
             }
-            currentEatObject = other.gameObject;
-            other.gameObject.SetActive(false);
+            currentEatObject = collision.gameObject;
+            collision.gameObject.SetActive(false);
             await UniTask.Delay(1000);
-            other.gameObject.SetActive(true);
-            if(other.gameObject.TryGetComponent<Rigidbody>(out var rigidbody)) {
-                rigidbody.AddForce(Vector3.left * 10, ForceMode.Impulse);
+            collision.gameObject.SetActive(true);
+            if(collision.gameObject.TryGetComponent<Rigidbody>(out var rigidbody)) {
+                collision.transform.position = transform.position + Vector3.left * 2;
+                rigidbody.AddForce(force * Time.deltaTime * Vector3.left, ForceMode.Impulse);
             }
             currentEatObject = null;
         }
