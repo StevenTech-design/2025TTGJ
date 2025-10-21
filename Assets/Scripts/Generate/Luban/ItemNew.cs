b7ad1efa194c0db2136a350c1e52a80e5ac8238f
THL7756 <z775623431@gmail.com>
2025-10-21 21:29:23 +0800

    道具表修改

--- Assets/Scripts/Generate/Luban/ItemNew.cs
+++ Assets/Scripts/Generate/Luban/ItemNew.cs
@@ -23,10 +23,7 @@ public sealed partial class ItemNew : Luban.BeanBase
         { if(!_buf["itemType"].IsNumber) { throw new SerializationException(); }  ItemType = _buf["itemType"]; }
         { if(!_buf["model"].IsString) { throw new SerializationException(); }  Model = _buf["model"]; }
         { if(!_buf["model_size"].IsNumber) { throw new SerializationException(); }  ModelSize = _buf["model_size"]; }
-        {
-        	var __evo = _buf["evo_id"];
-        	EvoId = (__evo != null && __evo.IsNumber) ? __evo : 0; // tolerate missing evo_id by defaulting to 0
-        }
+        { if(!_buf["evo_id"].IsNumber) { throw new SerializationException(); }  EvoId = _buf["evo_id"]; }
         { if(!_buf["drawing"].IsNumber) { throw new SerializationException(); }  Drawing = _buf["drawing"]; }
         { if(!_buf["collision"].IsNumber) { throw new SerializationException(); }  Collision = _buf["collision"]; }
         { if(!_buf["lift"].IsNumber) { throw new SerializationException(); }  Lift = _buf["lift"]; }
@@ -41,6 +38,9 @@ public sealed partial class ItemNew : Luban.BeanBase
         { if(!_buf["maturity_Effect"].IsString) { throw new SerializationException(); }  MaturityEffect = _buf["maturity_Effect"]; }
         { if(!_buf["if_eat"].IsNumber) { throw new SerializationException(); }  IfEat = _buf["if_eat"]; }
         { if(!_buf["eat_Effect"].IsString) { throw new SerializationException(); }  EatEffect = _buf["eat_Effect"]; }
+        { if(!_buf["if_popcorn"].IsNumber) { throw new SerializationException(); }  IfPopcorn = _buf["if_popcorn"]; }
+        { if(!_buf["if_hidden_door"].IsNumber) { throw new SerializationException(); }  IfHiddenDoor = _buf["if_hidden_door"]; }
+        { if(!_buf["if_lake"].IsNumber) { throw new SerializationException(); }  IfLake = _buf["if_lake"]; }
     }
 
     public static ItemNew DeserializeItemNew(JSONNode _buf)
@@ -91,7 +91,7 @@ public sealed partial class ItemNew : Luban.BeanBase
     /// <summary>
     /// 物品举高时的高度
     /// </summary>
-    public readonly int Height;
+    public readonly float Height;
     /// <summary>
     /// 是否可以被门吞
     /// </summary>
@@ -132,6 +132,18 @@ public sealed partial class ItemNew : Luban.BeanBase
     /// 吞下时效果
     /// </summary>
     public readonly string EatEffect;
+    /// <summary>
+    /// 是否会被爆米花机吃
+    /// </summary>
+    public readonly int IfPopcorn;
+    /// <summary>
+    /// 是否会被隐藏门吃
+    /// </summary>
+    public readonly int IfHiddenDoor;
+    /// <summary>
+    /// 是否会被湖神吃
+    /// </summary>
+    public readonly int IfLake;
    
     public const int __ID__ = -514125459;
     public override int GetTypeId() => __ID__;
@@ -164,6 +176,9 @@ public sealed partial class ItemNew : Luban.BeanBase
         + "maturityEffect:" + MaturityEffect + ","
         + "ifEat:" + IfEat + ","
         + "eatEffect:" + EatEffect + ","
+        + "ifPopcorn:" + IfPopcorn + ","
+        + "ifHiddenDoor:" + IfHiddenDoor + ","
+        + "ifLake:" + IfLake + ","
         + "}";
     }
 }
