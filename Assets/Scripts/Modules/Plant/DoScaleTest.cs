870d81ff88b859a2f6d92c96da8f9779666a1c77
DESKTOP-CCL570M\-BBBBB- <m18342211892@163.com>
2025-10-23 17:55:43 +0800

    作物动画

---
+++ Assets/Scripts/Modules/Plant/DoScaleTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DoScaleTest : MonoBehaviour
+{
+   
+    void Start()
+    {
+
+    }
+
+   
+    void Update()
+    {
+       
+        
+
+    }
+
+   
+
+           
+            
+}
--- Assets/Scripts/Modules/Plant/PlantBase.cs
+++ Assets/Scripts/Modules/Plant/PlantBase.cs
@@ -45,13 +45,33 @@ namespace TTGJ.Plant
             if (currentGrouthCount == willOccupiedFieldSize)
             {
                 ++currentGrouthCount;
-                transform.DOScale(ConfigManager.Instance.GetPlantGrowthModelSize(itemType, currentGrouthCount), 0.5f);
+                
+                // 成熟动画1
+                // transform.DOScale(ConfigManager.Instance.GetPlantGrowthModelSize(itemType, currentGrouthCount), 0.5f);
+                // 成熟动画2
+                // transform.DOScale(ConfigManager.Instance.GetPlantGrowthModelSize(itemType, currentGrouthCount), 0.5f).SetEase(Ease.OutElastic);
+                
+                // 生长动画3 + 成熟动画
+                Sequence mySequence = DOTween.Sequence();
+                mySequence.Append(transform.DOScale(1.5f, 1f)); //生长阶段
+                mySequence.Append(transform.DOScale(new Vector3(2, 2, 2), 0.5f).SetEase(Ease.OutElastic)); //成熟时刻
+                
+              
+                // 落地动画
+                // transform.DOPunchScale(new Vector3(-1.0f, 0.5f,-1.0f), 0.2f);
             }
             else if (FieldSystem.Instance.ToOccupied(GetComponent<Cell>().cellPos, currentOccupiedFieldSize, willOccupiedFieldSize))
             {
                 ++currentGrouthCount;
-                transform.DOScale(ConfigManager.Instance.GetPlantGrowthModelSize(itemType, currentGrouthCount), 0.5f);
+                // transform.DOScale(ConfigManager.Instance.GetPlantGrowthModelSize(itemType, currentGrouthCount), 0.5f);
+                
+                // 生长动画3 + 成熟动画
+                Sequence mySequence = DOTween.Sequence();
+                mySequence.Append(transform.DOScale(1.5f, 1f)); //生长阶段
+                mySequence.Append(transform.DOScale(new Vector3(2, 2, 2), 0.5f).SetEase(Ease.OutElastic)); //成熟时刻
+
             }
+            
 
             CheckEvolution();
         }
