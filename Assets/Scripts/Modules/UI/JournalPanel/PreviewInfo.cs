ba862ae71cf2ca5262e64c95c71de24273c338a1
Steven <1469359779@qq.com>
2025-10-27 23:54:58 +0900

    fix render model

--- Assets/Scripts/Modules/UI/JournalPanel/PreviewInfo.cs
+++ Assets/Scripts/Modules/UI/JournalPanel/PreviewInfo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using TTGJ.Common;
 using TTGJ.Framework;
 using TTGJ.Luban;
 using TTGJ.Plant;
@@ -30,11 +31,17 @@ namespace TTGJ.UI
             _name.text = item.Name;
             _description.text = item.ItemTip;
             GameObject model = Instantiate(StResources.Instance.LoadByResources<GameObject>(item.Model), _modelRoot.transform);
-            if(model != null) {
-                GameObject go = Instantiate(model, _modelRoot.transform);
-                go.transform.localScale = new Vector3(item.ModelSize, item.ModelSize, item.ModelSize);
-                go.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            if(model == null) {
+                return;
             }
+            if (model.TryGetComponent<Rigidbody>(out var rigidbody)) { 
+                Destroy(rigidbody);
+            }
+            if (model.TryGetComponent<Collider>(out var collider)) { 
+                Destroy(collider);
+            }
+            model.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            LayerUtility.ChangeLayerRecursively(model, LayerMask.NameToLayer("RenderModel"));
         }
 
 
