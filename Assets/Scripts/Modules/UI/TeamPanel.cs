03f70fa9e5dc551568fe40d9700afe0b374bf61f
Steven <1469359779@qq.com>
2025-10-29 01:27:38 +0900

    fix panel

---
+++ Assets/Scripts/Modules/UI/TeamPanel.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+namespace TTGJ.UI
+{
+    public class TeamPanel : UIPanel
+    {
+        [SerializeField] private Button _exitBtn;
+    
+
+        private void OnEnable()
+        {
+            _exitBtn.onClick.AddListener(OnExitBtnClick);
+        
+        }
+
+        private void OnDisable()
+        {
+            _exitBtn.onClick.RemoveListener(OnExitBtnClick);
+        
+        }
+
+        private void OnExitBtnClick()
+        {
+            UIManager.Instance.PopUp();
+        }
+    }
+}
